c5a81da1e2239cb7161b125087c165f7beedb78b
EnguerrandF <feignon.enguerrand@outlook.fr>
2023-06-06 14:52:13 +0200

    Start projet

---
+++ epic_events/app_clients_contract_event/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppClientsContractEventConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_clients_contract_event'
